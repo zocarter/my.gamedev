7bafc9c11e392115c0c7203a9a51db4e3d029188
zocarter <missionbit@missionbits-MacBook-Pro-10.local>
2016-07-21 09:02:10 -0700

    this is what i did so far

--- state0.js
+++ state0.js
@@ -1,51 +1,69 @@
 var demo = {};
-var speed = 3;
 var skunk;
-//var platforms
+var obsticles;
 
 demo.state0 = function(){};
 
-demo.state0.prototype = { 
+demo.state0.prototype = {
+    
  preload: function(){
-game.load.image('city', "cityBG.png");
-game.load.spritesheet('skunk', 'skunk.png');
-    //asset
-     game.load.image('car', "carredch.png");
-     game.load.image('trash', "trash.png");
-     game.load.image('cup', "cup.png");
+     game.load.image('city', "cityBG.png");
      
-    },
+     game.load.image('skunk', 'skunk2.png');
+     game.load.image('trash', 'trash.png');
+     game.load.image('car', 'civ_car.png');
+     game.load.image('taxi', 'taxi.png');
+ }, 
     
-    create: function(){
-       game.world.setBounds(0,0,2183,1000);
-       game.physics.startSystem(Phaser.Physics.ARCADE);
+create: function(){
+        game.world.setBounds(0, 0, 2183, 920);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         
-    
-   var bg = game.add.sprite(0, 0, 'city');  
-     bg.height = game.height;
-     bg.width = game.width;
+        var bg = game.add.sprite(0,0,'city');
+        bg.height = game.height;
+        bg.width = game.width;
         
-    skunk = game.add.sprite(0, 600, 'skunk');
-        
+        skunk = game.add.sprite(59, 600, 'skunk');
+       
         game.physics.enable(skunk);
-      
-        skunk.body.gravity.y = 600;
-   
+        skunk.body.gravity.y = 900;
         skunk.body.collideWorldBounds = true;
+        skunk.anchor.setTo(.5,.5);
+        skunk.scale.setTo(-.15,.15);
         
-        skunk.scale.setTo(2,2);
-     
-    
-    var spaceKey = game.input.keyboard.addKey(
-    Phaser.Keyboard.SPACEBAR);
-        spaceKey.onDown.add(this.jump, this);
-      
+        obstacles = game.add.group();
+        obstacles.enableBody = true;
+         
+        var car = obstacles.create(800, 880, 'car');
+        car.body.immovable = true;
+         
+ 
+        var taxi = obstacles.create(1800,870,'yellow');
+        taxi.body.immovable = true; 
+        taxi.scale.setTo(0.8,0.8);
+         
+        var trash = obstacles.create(2500, 880,'trash');
+         
+        cursors = game.input.keyboard.createCursorKeys();
+        this.spacekey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     },
     
-  //  update: function
-
-    
-    };
+ update: function(){
+         game.physics.arcade.collide(obstacles, skunk);
+         obstacles.x -=2;
+         if(this.spacekey.isDown){
+             skunk.body.velocity.y = -300;
+         
+       }
+    },
+     
+checkOverlap: function(spriteA, spriteB) {
+        var boundsA = spriteA.getBounds();
+        var boundsB = spriteB.getBounds();
+        return Phaser.Rectangle.intersects(boundsA, boundsB);
+       
+   },
+};
 
     
     
