b82a28a968ececdb0b943b79b4a88b187ac74a71
zocarter <missionbit@missionbits-MacBook-Pro-10.local>
2016-07-21 16:38:48 -0700

    final

--- state0.js
+++ state0.js
@@ -1,6 +1,6 @@
 var demo = {};
 var skunk;
-var obsticles;
+var obstacles;
 
 demo.state0 = function(){};
 
@@ -11,8 +11,8 @@ demo.state0.prototype = {
      
      game.load.image('skunk', 'skunk2.png');
      game.load.image('trash', 'trash.png');
-     game.load.image('car', 'civ_car.png');
-     game.load.image('taxi', 'taxi.png');
+     game.load.image('car', 'Car_Purple_Side.svg');
+     game.load.image('taxi', 'Pixel_Car.png');
  }, 
     
 create: function(){
@@ -29,7 +29,7 @@ create: function(){
         skunk.body.gravity.y = 900;
         skunk.body.collideWorldBounds = true;
         skunk.anchor.setTo(.5,.5);
-        skunk.scale.setTo(-.15,.15);
+        skunk.scale.setTo(.15,.15);
         
         obstacles = game.add.group();
         obstacles.enableBody = true;
@@ -38,7 +38,7 @@ create: function(){
         car.body.immovable = true;
          
  
-        var taxi = obstacles.create(1800,870,'yellow');
+        var taxi = obstacles.create(1800,870,'taxi');
         taxi.body.immovable = true; 
         taxi.scale.setTo(0.8,0.8);
          
@@ -50,11 +50,20 @@ create: function(){
     
  update: function(){
          game.physics.arcade.collide(obstacles, skunk);
-         obstacles.x -=2;
+          obstacles.x -=2;
          if(this.spacekey.isDown){
              skunk.body.velocity.y = -300;
          
        }
+     if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+         {
+             skunk.x = skunk.x + 3;
+         }
+     
+     if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+         {
+             skunk.x = skunk.x - 3;
+         }
     },
      
 checkOverlap: function(spriteA, spriteB) {
